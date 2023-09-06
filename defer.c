483e76d4bae8629d8b073f5181b97ac49945ddc6
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2023-09-06 11:46:49 -0400

    starting phase

---
+++ defer.c
@@ -0,0 +1,86 @@
+#include "defer.h"
+
+int ends_with_c(const char * string); 
+int ends_with_star_c(const char * string); 
+
+int main(int argc, char ** argv)
+{  
+
+    int err_count = 0;
+    char * err_string = malloc(sizeof(char)); 
+    char * temp = malloc(sizeof(char) * 100); 
+
+    for (int i = 1; i < argc; i++)
+    { 
+        if (ends_with_star_c(argv[i]))
+        {
+            /*
+                Go into system and generate file list of all the .c files in that 
+            */
+        }
+        else if (ends_with_c(argv[i]))
+        {
+
+           /*
+                Open and parse that specific file.  
+           */ 
+        }
+        else
+        {
+            err_count += 1; 
+            sprintf(temp, "%sARGUMENT %d (%s) INVALID. Not a .c or .cc file.\n%s", RED, i, argv[i], DEFAULT); 
+            err_string = realloc(err_string, sizeof(char) * (100 * err_count)); 
+            strcat(err_string, temp); 
+            
+        }
+    }
+
+
+    FILE * fptr = fopen(COMPILE_FILE, "r"); 
+
+    
+
+
+    fclose(fptr);
+
+
+
+    printf("%s", err_string);  
+    
+    free(err_string); 
+    free(temp); 
+
+    return 0; 
+}
+
+
+int ends_with_c(const char * string)
+{   
+    int len = strlen(string);  
+
+    if (len < 3)
+    {
+        return 0;
+    }
+
+    int test_1 = (string[len - 2] == '.' && string[len - 1] == 'c'); 
+    int test_2 = (string[len - 3] == '.' && string[len - 2] == 'c' && string[len - 1] == 'c');  
+
+    return (test_1 || test_2); 
+}
+
+int ends_with_star_c(const char * string)
+{
+    int len = strlen(string); 
+    
+    if (len < 3)
+    {
+        return 0; 
+    }
+
+    int test_1 = (string[len - 3] == '*' && string[len - 2] == '.' && string[len - 1] == 'c'); 
+    int test_2 = (string[len - 4] == '*' && string[len - 3] == '.' && string[len - 2] == 'c' && string[len - 1] == 'c');  
+    
+    return (test_1 || test_2);  
+}
+
---
+++ defer.h
@@ -0,0 +1,13 @@
+#ifndef DEFER_H 
+#define DEFER_H
+
+#define RED "\033[31m"
+#define DEFAULT "\033[39m"
+#define COMPILE_FILE "command_t.txt"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif
