e2a95c090cf32bfce52280127f24126b977743a9
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2023-09-15 15:18:46 -0400

    restart to make use of pthreads

--- defer.h
+++ defer.h
@@ -12,34 +12,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-
-typedef struct pair
-{
-    int first; 
-    int second; 
-} pair; 
-
-typedef struct scope_struct 
-{
-    pair * pairs; 
-    int count; 
-} scope_struct; 
-
-typedef struct revert_info
-{
-    char * filename; 
-    char * content; 
-} revert_info; 
-
-
-void modify_file(const char *); 
-void revert_file(const char *, revert_info *, int); 
-void strip_whitespace(char *); 
-void find_scopes(const char *, scope *); 
-void find_and_replace_defer(const char *, pair *);
-
-int ends_with(const char *, const char *); 
-int end_of_scope(int, scope_struct *); 
-unsigned int get_file_size(FILE *); 
+#include <pthread.h>
 
 #endif
