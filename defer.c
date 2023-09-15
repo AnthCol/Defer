341a37b88b96a54b2ca5057d5b00bee7e6207a3e
Anthony Colaiacovo <anthonyecolaiacovo@gmail.com>
2023-09-15 17:15:18 -0400

    progress

--- defer.c
+++ defer.c
@@ -3,11 +3,165 @@
  
 int main(int argc, char ** argv)
 {
+    int size; 
+    int file_count = 0;
+    char buf [512]; 
+    FILE * fptr; 
+    char * temp = malloc(1);
+    revert_pair * original_data = malloc(1); 
+
+
+    
+    if (strcmp(argv[1], "**") == 0)
+    {
+        /*
+            In this case, search all subdirectories from
+            the current directory and make the list of files. 
+        */
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+
+            if (!already_processed(original_data, argv[i], file_count))
+            {     
+                fptr = fopen(argv[i], "r"); 
+                if (fptr == NULL)
+                {
+                    printf("%sCould not open file:%s %s\n", RED, DEFAULT, argv[i]); 
+                }
+                else
+                {
+                    copy_file_data(fptr, argv[i], original_data, file_count); 
+                    file_count += 1; 
+                    fclose(fptr); 
+                }
+            }
+        }
+    }
+
+
+    pthread_t * thread_handles; 
+    thread_handles = malloc(file_count * sizeof(pthread_t)); 
+
+    for (int i = 0; i < file_count; i++)
+    {
+        pthread_create(&thread_handles[i], NULL, modify_file, (void *) &original_data[i]); 
+    }
+
+    for (int i = 0; i < file_count; i++)
+    {
+        pthread_join(thread_handles[i], NULL); 
+    }
 
 
 
+    fptr = fopen(COMPILE_FILE, "r"); 
+    size = get_file_size(fptr); 
+    temp = realloc(temp, size * 2); 
+    fread(temp, sizeof(char), size, fptr); 
+    fclose(fptr); 
 
+    // FIXME verify compiling file for sudo and rm
 
-
+    FILE * compile_process = popen(temp, "r"); 
+    while (fgets(buf, sizeof(buf), compile_process) != NULL)
+    {
+        printf("%s", buf); 
+    }
+    pclose(compile_process); 
+    free(temp); 
+    
+    
+    for (int i = 0; i < file_count; i++)
+    {
+        pthread_create(&thread_handles[i], NULL, revert_file, (void*) &original_data[i]); 
+        free(original_data[i].filename); 
+        free(original_data[i].contents); 
+    }
+    for (int i = 0; i < file_count; i++)
+    {
+        pthread_join(thread_handles[i], NULL); 
+    }
+    free(thread_handles);  
+    free(original_data); 
+    
     return 0; 
+}
+
+void * modify_file (void * modify_data)
+{
+
+
+
+    return NULL; 
+}
+
+void * revert_file (void * revert_data)
+{
+
+    FILE * fptr = fopen(revert_data.filename, "w"); 
+
+
+
+
+    fclose(fptr); 
+
+    return NULL;  
+}
+
+void copy_file_data(FILE * fptr, const char * filename, revert_pair * original_data, int index)
+{
+
+    int name_len = strlen(filename); 
+    int file_len = get_file_size(fptr); 
+
+    original_data = realloc(original_data, sizeof(revert_pair) * (index + 1)); 
+    original_data[index].filename = malloc(name_len + 1); 
+    original_data[index].contents = malloc(file_len + 1); 
+
+    char * buffer = malloc(file_len + 1); 
+    fread(buffer, sizeof(char), file_len, fptr); 
+
+    strncpy(original_data[index].filename, filename, name_len); 
+    strncpy(original_data[index].contents, buffer, file_len);
+
+    return; 
+}
+
+
+int ends_with(const char * string, const char * end)
+{
+
+   int len = strlen(string) - 1;  
+   int end_len = strlen(end) - 1; 
+
+    if (end_len < len)
+    {
+        return 0; 
+    }
+
+
+    while (end_len >= 0)
+    {
+        if (string[len] != end[end_len])
+        {
+            return 0; 
+        }
+        
+        len -= 1; 
+        end_len -= 1; 
+    }
+
+    return 1; 
+}
+
+int get_file_size(FILE * fptr)
+{
+    int size; 
+    fseek(fptr, 0L, SEEK_END); 
+    size = ftell(fptr); 
+    fseek(fptr, 0L, SEEK_SET); 
+    return size; 
 }
--- defer.h
+++ defer.h
@@ -3,15 +3,29 @@
 
 #define _GNU_SOURCE
 #define _POSIX_C_SOURCE 200809L
+#define MAX_THREADS 64
 #define RED "\033[31m"
 #define PURPLE "\033[35m"
 #define DEFAULT "\033[39m"
 #define COMPILE_FILE "command.defer"
 
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+
+
+typedef struct revert_pair
+{ 
+    char * filename; 
+    char * contents;  
+} revert_pair; 
+
+int ends_with(const char *, const char *); 
+int already_processed(revert_pair *, const char *, int); 
+int get_file_size(FILE *); 
+void copy_file_data(FILE *, const char *, revert_pair *, int); 
+void * modify_file(void * ); 
+void * revert_file(void * ); 
 
 #endif
